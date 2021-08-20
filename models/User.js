8d0fa992af9395a4ad6e32de8da7430de92db3a1
Wissal Elaagueb <Wissalagueb@gmail.com>
2021-08-20 19:36:10 +0100

    fixed response format and added Joi validation

--- models/User.js
+++ models/User.js
@@ -1,6 +1,9 @@
+const Joi = require("joi").extend(require('@joi/date'));
 const mongoose = require("mongoose");
+Joi.objectId = require('joi-objectid')(Joi);
 
-module.exports = mongoose.model(
+
+const User = mongoose.model(
   "User",
   new mongoose.Schema({
     username: {
@@ -45,3 +48,33 @@ module.exports = mongoose.model(
     },
   })
 );
+
+module.exports.User = User;
+
+function validateUser (user) {
+    const schema = Joi.object ({
+        username : Joi.string().min(5).max(50).required(),
+        email : Joi.string().min(5).max(50).required().email().required(),
+        password : Joi.string().min(5).max(255).required(),
+        firstName: Joi.string().min(5).max(50).required(),
+        lastName: Joi.string().min(5).max(50).required(),
+        role: Joi.string().valid("normal", "moderator", "admin").required(),
+        address: Joi.string(),
+        phoneNumber: Joi.number().min(10000000).max(99999999).message('PhoneNumber must be 8 digits.'),
+        gender: Joi.string().valid('male', 'female'),
+        birthDate: Joi.date().format('YYYY-MM-DD').min("1900-01-01"),
+        avatar: Joi.string()
+    });
+
+    return schema.validate(user);
+}
+
+function validateUserId(userId) {
+    const schema = Joi.object ({
+        id : Joi.objectId().message('Invalid id - '+ userId.id)
+    });
+    return schema.validate(userId);
+}
+
+module.exports.validateUser = validateUser;
+module.exports.validateUserId = validateUserId;
--- routes/user.js
+++ routes/user.js
@@ -1,87 +1,123 @@
-const User = require("../models/User")
+const {User,  validateUser, validateUserId}= require('../models/User')
 var express = require("express");
 var router = express.Router();
-const _ = require('lodash')
+const _ = require('lodash');
+
+
+
+function failureResponse(error){
+   const response = new Object();
+   response.status="Failure";
+   response.error = {message : error};
+   return response;
+}
+
+function successResponse(user){
+  const response = new Object();
+  response.status="Success";
+  response.data = user;
+  return response;
+}
+
 
 
 router.get("/", async (req, res)=> {
-        res.send(await User.find());
+    res.send(await User.find());
 }) 
 
-
+ 
 router.get("/:id", async (req, res) => {
     try{
+
+        const {error} = validateUserId(req.params);
+        if (error)
+            return res.status(400).send(failureResponse(error.details[0].message));
+
         const user= await User.findById(req.params.id);
-      
         if(!user)
-          res.status(404).send("user not found.")
+            return res.status(404).send(failureResponse("User id not found - "+req.params.id));       
        
-       res.send(user)
+        res.send(successResponse(user));      
     }
     catch(ex){
-      res.status(400).send(ex.message)
+        res.send(failureResponse( ex.message));    
     }
 }) 
 
 
-router.delete("/:id", async  (req, res) => {
-  
-    try {
-        const user = await User.findByIdAndremove(req.params.id);
-        
-        if (!user)
-           return res.status(404).send("This ID does not belong to any user");
-        
-        res.send("user deleted with succes")   
-
-    }catch(ex){
-        
-        res.status(400).send(ex.message)
-    }
-  
-}) 
 
 
 router.post("/", async (req, res) => {
     try{
+        const {error} = validateUser(req.body);
+        console.log(error)
+        if (error)
+            return res.status(400).send(failureResponse(error.details[0].message));
 
         let user = await User.findOne({email :req.body.email});
         if (user)
-           return res.status(400).send("User already registred");
+            return res.status(400).send(failureResponse("User already registred"));
 
         user = new User(_.pick(req.body,[
-            'username','email','password','firstName','lastName','role',
-            'adress','phoneNumber','gender','birthDate','avatar'
+              'username','email','password','firstName','lastName','role',
+              'adress','phoneNumber','gender','birthDate','avatar'
         ]))
-
-        const result =await user.save();
-        res.send(user)
+ 
+        await user.save();
+        res.send(successResponse(user));   
     }
-   catch(ex){
-       
-     res.status(400).send(ex.message);
-
-   }
+    catch (ex){
+        res.send(failureResponse( ex.message));  
+    }
+    
 }) 
 
 
 router.put("/:id", async (req,res) =>{
-     try{       
+     try{     
+        var {error} = validateUserId(req.params);
+        if (error)
+            return res.status(400).send(failureResponse(error.details[0].message));
       
+        var {error} = validateUser(req.body);
+        if (error)
+            return res.status(400).send(failureResponse(error.details[0].message));
+
         const user = await User.findByIdAndUpdate(req.params.id,_.pick(req.body,[
         'username','email','password','firstName','lastName','role',
         'adress','phoneNumber','gender','birthDate','avatar'
          ]));
       
         if (!user)
-          res.status(404).send("This ID does not belong to any user")
+            return res.status(404).send(failureResponse("User id not found - "+req.params.id));  
 
-        res.send("User updated with succes")
-    
-    }catch(ex){
-        res.status(400).send(ex.message)
+        res.send(successResponse(user));
+    }
+    catch(ex){
+        res.send(failureResponse( ex.message));  
      }
 })
+
+ 
+router.delete("/:id", async  (req, res) => {
+    try {
+        const {error} = validateUserId(req.params);
+        if (error)
+            return res.status(400).send(failureResponse(error.details[0].message));
+
+        const user = await User.findByIdAndRemove(req.params.id);
+        
+        if (!user)
+            return res.status(404).send(failureResponse("User id not found - "+req.params.id)); 
+        
+        res.send(successResponse(user));   
+    }
+    catch(ex){    
+        res.send(failureResponse( ex.message)); 
+    }
+  
+}) 
+
 
 
 
