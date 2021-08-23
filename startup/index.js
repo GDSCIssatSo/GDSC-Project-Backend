19e2097483768b4d3a63ab40ccc3afdbae7730ed
zahreddine <zahreddine.atoui@issatso.u-sousse.com>
2021-08-23 16:00:43 +0100

    feat : add swagger documentation

--- startup/index.js
+++ startup/index.js
@@ -2,6 +2,7 @@ const morgan = require("morgan");
 
 module.exports = (app) => {
   app.use(morgan("common"));
+  require("./swaggerDocs")(app);
   require("./routes")(app);
   require("./db")();
 };
